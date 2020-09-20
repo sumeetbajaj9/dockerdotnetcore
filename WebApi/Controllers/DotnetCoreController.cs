e4e5ecebae9147de02f8754e54737366e6ffad25
Sumeet Bajaj <sumeetbajaj@Sumeets-MacBook-Pro.local>
2020-09-20 23:20:21 +0530

    initial commit

---
+++ WebApi/Controllers/DotnetCoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DotnetCoreController : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return ("Hi");
+        }
+
+        [HttpGet,Route("helloworld")]
+        public string Get()
+        {
+            return ("Hello World from Dotnet Core ");
+        }
+    }
+}
